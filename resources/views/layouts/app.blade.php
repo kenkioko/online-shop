8ddda2e67f8d38a8a151b36a1bc21513c560da5b
Kioko Kennedy <kmkioko@gmail.com>
2020-01-14 23:23:00 +0300

    links edit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,7 +17,7 @@
         <link rel="stylesheet" href="{{ asset('/css/app.css') }}">
 
         <!-- fontawesome -->
-        <link rel="stylesheet" href="{{ asset('/fontawsome/css/all.min.css') }}">
+        <link rel="stylesheet" href="{{ asset('/fontawesome/css/all.min.css') }}">
     @show
 
     <title>@yield('title')</title>
